41b7c426944f1003768741f36c36b10ad5927db3
JessyIsCute <146076210+JessyIsCute@users.noreply.github.com>
2025-06-11 14:46:02 +0200

    test

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+ let button = document.getElementById("button");
+    button.addEventListener("click", increaseCount)
+
+const increaseCount = () => {
+     let count = document.getElementById("counter");
+     count++
+}
