72efef9bcc8cd4d57035d5d23655d13547535307
JessyIsCute <lord.vraxxer@gmail.com>
2025-06-13 14:03:20 +0200

    Added basic layout and water concept

--- scripts/main.js
+++ scripts/main.js
@@ -1,52 +1,87 @@
  const global = {
      table: [],
-     count: 0
- } //kikj]j
+     mapWidth: 10,
+     mapHeight: 10,
+     count: 0,
+     currentLocation: { x: 0, y: 0 }
+
+ } 
 
  window.addEventListener("DOMContentLoaded", () => {
-     let button = document.getElementById("red");
-     button.addEventListener("click", increaseCount);
+     let waterButton = document.getElementById("waterButton");
+     waterButton.addEventListener("click", increaseCount);
      createTable();
+
+     let buttonup = document.getElementById("buttonup");
+     let buttondown = document.getElementById("buttondown");
+     let buttonleft = document.getElementById("buttonleft");
+     let buttonright = document.getElementById("buttonright");
+     buttonup.addEventListener("click",  () => movePlayer(buttonup))
+     buttondown.addEventListener("click", () => movePlayer(buttondown))
+     buttonleft.addEventListener("click", () => movePlayer(buttonleft))
+     buttonright.addEventListener("click", () => movePlayer(buttonright))
  }
 );
 
 const increaseCount = () => {
-     let count = document.getElementById("counter");
-     count++
-     console.log(global.table);
+     let water = document.getElementById("water");
+     let waterValue = parseInt(water.innerText, 10);
+     let waterIncrement = 1;
+     water.innerText = waterValue + waterIncrement;
+     
+     console.log(water);
 }
 const createTable = () => {
      let table = document.createElement("table");
      table.id = "myTable";
      let world = document.getElementById("world")
      world.appendChild(table);
- 
+
      
-    
-     
-     for (let y = 0; y < 10; y++) {
+     for (let y = 0; y < global.mapHeight; y++) {
           
 
           let row = document.createElement("tr");
           table.appendChild(row);
           
 
-          for (let x = 0; x < 10; x++) {
+          for (let x = 0; x < global.mapWidth; x++) {
                let cell = document.createElement("td");
-               cell.textContent = `${x} ${y}`;
-               row.appendChild(cell);
-               cell.classList.add("worldspace");
-
-
-
 
                worldspace = {
                 cell: cell,
                 x: x,
                 y: y,
+                celltype: "grass"
                };
+
+               cell.textContent = `${x} ${y} ${worldspace.celltype} "\n"`;
+               row.appendChild(cell);
+               cell.classList.add("worldspace");
+
+               
 
                global.table.push(worldspace);
           }
      }
+}
+
+const movePlayer = (direction) => {
+     let location = global.currentLocation;
+     console.log(global.table[location.x+location.y*global.mapWidth])
+     global.table[location.x + location.y * global.mapWidth].cell.classList.remove("player");
+
+     if (direction.id === "buttonup") {
+          location.y = Math.max(0, location.y - 1);
+     } else if (direction.id === "buttondown") {
+          location.y = Math.min(global.mapHeight - 1, location.y + 1);
+     } else if (direction.id === "buttonleft") {
+          location.x = Math.max(0, location.x - 1);
+     } else if (direction.id === "buttonright") {
+          location.x = Math.min(global.mapWidth - 1, location.x + 1);
+     }
+     
+     global.table[location.x + location.y * global.mapWidth].cell.classList.add("player");
+     let newLocation = { x: location.x, y: location.y };
+     
 }
